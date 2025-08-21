4957e11a0692e3ac6bb73e90e8156257ef9a2437
HenriqueWegner <HenriqueWegner@github.com>
2025-08-21 12:07:07 -0300

    Start project

---
+++ src/main/java/io/github/henriquewegner/EcommerceOrderServiceApi/EcommerceOrderServiceApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.henriquewegner.EcommerceOrderServiceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOrderServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOrderServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/henriquewegner/EcommerceOrderServiceApi/EcommerceOrderServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.henriquewegner.EcommerceOrderServiceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOrderServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
