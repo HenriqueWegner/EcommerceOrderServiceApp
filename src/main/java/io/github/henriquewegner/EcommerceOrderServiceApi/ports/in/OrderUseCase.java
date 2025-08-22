bb4db1c6b9fa2e74ecc335db3b2a3a865f0e89c1
HenriqueWegner <HenriqueWegner@github.com>
2025-08-21 23:15:36 -0300

    fix: separated repository from mapper

--- src/main/java/io/github/henriquewegner/EcommerceOrderServiceApi/ports/in/OrderUseCase.java
+++ src/main/java/io/github/henriquewegner/EcommerceOrderServiceApi/ports/in/OrderUseCase.java
@@ -2,8 +2,9 @@ package io.github.henriquewegner.EcommerceOrderServiceApi.ports.in;
 
 import io.github.henriquewegner.EcommerceOrderServiceApi.domain.model.Order;
 import io.github.henriquewegner.EcommerceOrderServiceApi.infrastructure.persistence.OrderEntity;
+import io.github.henriquewegner.EcommerceOrderServiceApi.web.dto.request.OrderRequestDTO;
 
 public interface OrderUseCase {
 
-    void createOrder(Order order);
+    void createOrder(OrderRequestDTO orderDTO);
 }
