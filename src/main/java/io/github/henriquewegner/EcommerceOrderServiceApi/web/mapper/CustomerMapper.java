274e65fb72e3524c9ce36c86eeb1fdc8cc15e3d3
HenriqueWegner <HenriqueWegner@github.com>
2025-09-05 19:44:59 -0300

    fix: deleted remains of customer controller

--- src/main/java/io/github/henriquewegner/EcommerceOrderServiceApi/web/mapper/CustomerMapper.java
+++ src/main/java/io/github/henriquewegner/EcommerceOrderServiceApi/web/mapper/CustomerMapper.java
@@ -2,20 +2,15 @@ package io.github.henriquewegner.EcommerceOrderServiceApi.web.mapper;
 
 import io.github.henriquewegner.EcommerceOrderServiceApi.domain.model.Customer;
 import io.github.henriquewegner.EcommerceOrderServiceApi.infrastructure.persistence.entities.CustomerEntity;
-import io.github.henriquewegner.EcommerceOrderServiceApi.web.dto.request.CustomerRequestDTO;
-import io.github.henriquewegner.EcommerceOrderServiceApi.web.dto.response.CustomerOrdersResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = ShippingAddressMapper.class)
 public interface CustomerMapper {
 
-    Customer toDomain(CustomerRequestDTO dto);
-
     @Mapping(target = "orders", ignore = true)
     Customer toDomain(CustomerEntity entity);
 
     CustomerEntity toEntity(Customer customer);
 
-    CustomerOrdersResponseDTO toDto(Customer customer);
 }
